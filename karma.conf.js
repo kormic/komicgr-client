50d64c4b9f7f0dabd608d42daab10d22f938de15
Michael Kornelakis <ycamic@gmail.com>
2020-12-16 21:26:27 +0200

    Project rename and define assets path
    * replace project komicGR to komicgr
    * set assets path in tsconfig
    * use the assets path in post-card component
    * install npm modules

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/komicGR-client'),
+      dir: require('path').join(__dirname, './coverage/komicgr-client'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },

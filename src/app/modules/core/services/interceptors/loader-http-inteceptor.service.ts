bd9a608c7628bb10dc43e6e03237736c9fadfe84
Michael Kornelakis <michalis@cyberlogic.gr>
2021-01-07 01:12:47 +0200

    Redirect to login when token expires

--- src/app/modules/core/services/interceptors/loader-http-inteceptor.service.ts
+++ src/app/modules/core/services/interceptors/loader-http-inteceptor.service.ts
@@ -1,19 +1,29 @@
 import { Injectable } from '@angular/core';
-import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpErrorResponse } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
 import { LoaderService } from '../loader/loader.service';
-import { finalize } from 'rxjs/operators';
+import { catchError, finalize } from 'rxjs/operators';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoaderHttpInteceptorService implements HttpInterceptor {
 
-  constructor(private loaderService: LoaderService) { }
+  constructor(private loaderService: LoaderService, private jwtHelperService: JwtHelperService, private router: Router, private authService: AuthService) { }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     this.loaderService.showLoader();
-    return next.handle(req).pipe(finalize(() => this.loaderService.hideLoader()));
+
+    return next.handle(req).pipe(catchError((e: HttpErrorResponse) => {
+      if (e.status === 401) {
+        this.authService.logoutUser();
+        this.router.navigateByUrl('/login');
+      }
+      return Observable.throw(e);
+    }), finalize(() => this.loaderService.hideLoader()));
   }
 
 }
